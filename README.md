abb267c01348f884f7a183bde04d158b0cf0d88d
Christian Muehlhaeuser <muesli@gmail.com>
2017-08-06 22:31:40 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,40 @@
+sticker
+=======
+
+A Golang lib to generate placeholder images with text.
+
+## Installation
+
+Make sure you have a working Go environment. See the [install instructions](http://golang.org/doc/install.html).
+
+To install sticker, simply run:
+
+    go get github.com/muesli/sticker
+
+To compile it from source:
+
+    cd $GOPATH/src/github.com/muesli/sticker
+    go get -u -v
+    go build && go test -v
+
+## Example
+```go
+gen, err := sticker.NewImageGenerator(sticker.Options{
+    TTFPath:     "/usr/share/fonts/TTF/Roboto-Bold.ttf",
+    MarginRatio: 0.2,
+    Foreground: color.RGBA{0x96, 0x96, 0x96, 0xff},
+    Background: color.RGBA{0xcc, 0xcc, 0xcc, 0xff},
+})
+img, err := ph.NewPlaceholder("Lorem ipsum!", 400, 200)
+...
+```
+
+![example placeholder](example/lorem.png)
+
+## Development
+
+API docs can be found [here](http://godoc.org/github.com/muesli/sticker).
+
+[![Build Status](https://secure.travis-ci.org/muesli/sticker.png)](http://travis-ci.org/muesli/sticker)
+[![Coverage Status](https://coveralls.io/repos/github/muesli/sticker/badge.svg?branch=master)](https://coveralls.io/github/muesli/sticker?branch=master)
+[![Go ReportCard](http://goreportcard.com/badge/muesli/sticker)](http://goreportcard.com/report/muesli/sticker)
---
+++ sticker.go
@@ -0,0 +1,151 @@
+/*
+ * crunchy - find common flaws in passwords
+ *     Copyright (c) 2017, Christian Muehlhaeuser <muesli@gmail.com>
+ *
+ *   For license see LICENSE
+ */
+
+package sticker
+
+import (
+	"errors"
+	"image"
+	"image/color"
+	"image/draw"
+	"io/ioutil"
+	"strconv"
+
+	"github.com/golang/freetype"
+	"github.com/golang/freetype/truetype"
+	"golang.org/x/image/font"
+)
+
+const (
+	dpi         = 72.00
+	maxFontSize = 512.00
+)
+
+// ImageGenerator lets you generate images & placeholders
+type ImageGenerator struct {
+	options Options
+	font    *truetype.Font
+}
+
+// Options contains all the settings for an ImageGenerator
+type Options struct {
+	TTFPath     string
+	Foreground  color.RGBA
+	Background  color.RGBA
+	MarginRatio float64
+}
+
+var (
+	// ErrInvalidDimensions gets returned when the requested image has an invalid width or height
+	ErrInvalidDimensions = errors.New("values for width or height must be positive")
+)
+
+// NewImageGenerator returns a new ImageGenerator
+func NewImageGenerator(options Options) (*ImageGenerator, error) {
+	if options.Foreground.A == 0 {
+		options.Foreground = color.RGBA{0x96, 0x96, 0x96, 0xff}
+	}
+	if options.Background.A == 0 {
+		options.Background = color.RGBA{0xcc, 0xcc, 0xcc, 0xff}
+	}
+	if options.MarginRatio < 0 {
+		options.MarginRatio = 0.2
+	}
+
+	ttf, err := ioutil.ReadFile(options.TTFPath)
+	if err != nil {
+		return nil, err
+	}
+	f, err := freetype.ParseFont(ttf)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ImageGenerator{
+		font:    f,
+		options: options,
+	}, nil
+}
+
+// NewPlaceholder returns a placeholder image with the given text or, if text was
+// an empty string, with the image bounds in the form "800x600".
+func (p *ImageGenerator) NewPlaceholder(text string, width, height int) (image.Image, error) {
+	if width < 0 || height < 0 {
+		return nil, ErrInvalidDimensions
+	}
+	if width == 0 && height == 0 {
+		return nil, ErrInvalidDimensions
+	}
+
+	if width == 0 {
+		width = height
+	} else if height == 0 {
+		height = width
+	}
+	if text == "" {
+		text = strconv.Itoa(width) + " x " + strconv.Itoa(height)
+	}
+
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+
+	c := freetype.NewContext()
+	c.SetDPI(dpi)
+	c.SetFont(p.font)
+	c.SetSrc(image.NewUniform(color.RGBA{0, 0, 0, 0}))
+	c.SetDst(img)
+	c.SetClip(img.Bounds())
+	c.SetHinting(font.HintingNone)
+
+	// draw with scaled fontsize to get the real text extent
+	fontsize, actwidth := maxPointSize(text, p.font, c, img,
+		int(float64(width)*(1.0-p.options.MarginRatio)),
+		int(float64(height)*(1.0-p.options.MarginRatio)))
+
+	actheight := c.PointToFixed(fontsize/2.2) / 64
+	xcenter := (float64(width) / 2.0) - (float64(actwidth) / 2.0)
+	ycenter := (float64(height) / 2.0) + (float64(actheight) / 2.0)
+
+	// draw the background
+	draw.Draw(img, img.Bounds(), image.NewUniform(p.options.Background), image.ZP, draw.Src)
+
+	// draw the text
+	c.SetFontSize(fontsize)
+	c.SetSrc(image.NewUniform(p.options.Foreground))
+	c.SetDst(img)
+	_, err := c.DrawString(text, freetype.Pt(int(xcenter), int(ycenter)))
+	if err != nil {
+		return nil, err
+	}
+
+	return img, nil
+}
+
+// maxPointSize returns the maximum point size we can use to fit text inside width and height
+// as well as the resulting text-width in pixels
+func maxPointSize(text string, f *truetype.Font, c *freetype.Context, img image.Image, width, height int) (float64, int) {
+	// never let the font size exceed the requested height
+	fontsize := maxFontSize
+	for int(c.PointToFixed(fontsize)/64) > height {
+		fontsize -= 2
+	}
+
+	// find the biggest matching font size for the requested width
+	actwidth := width + 1
+	for actwidth > width {
+		fontsize -= 2
+		c.SetFontSize(fontsize)
+
+		textExtent, err := c.DrawString(text, freetype.Pt(0, 0))
+		if err != nil {
+			return 0, 0
+		}
+
+		actwidth = int(float64(textExtent.X) / 64)
+	}
+
+	return fontsize, actwidth
+}
